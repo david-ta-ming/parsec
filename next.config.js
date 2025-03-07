c2bacb9b60e9067724b7e562082356394e8a72d8
David Liou <david.liou@nih.gov>
2025-03-07 16:25:46 -0500

    Remove deprecated env settings in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,17 +3,6 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
-  // This will make server-side environment variables explicitly available
-  serverRuntimeConfig: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
-  // This makes environment variables available to both server and client
-  publicRuntimeConfig: {
-    // Add any PUBLIC env vars here - DO NOT put the OpenAI key here
-  },
 };
 
 module.exports = nextConfig;
